bc40dd97cc8a521ba77f4bc3459c8a00faa42e32
ijelizondo <ijelizondo@gmail.com>
2013-10-08 17:24:54 -0500

    0.4.2
    change _system to _self to read pdf in android assets

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ function initialize() {
 }
 
 function openPDF() {
-     var ref = window.open('files/programa.pdf', '_system', 'location=yes');
+     var ref = window.open('files/programa.pdf', '_self', 'location=yes');
      ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
      ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
      ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });

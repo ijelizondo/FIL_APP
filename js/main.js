dc6f89e9edaa6a8694b2ad0e54135d69be572955
ijelizondo <ijelizondo@gmail.com>
2013-10-08 19:39:45 -0500

    0.5
    Beta, Read pdf online

--- js/main.js
+++ js/main.js
@@ -52,11 +52,11 @@ function initialize() {
 }
 
 function openPDF() {
-     var ref = window.open(encodeURI('files/programa.pdf'), '_blank', 'location=yes');
-     ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
-     ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
-     ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-     ref.addEventListener('exit', function(event) { alert(event.type); });
+     var ref = window.open('http://docs.google.com/viewer?url=http%3A%2F%2Ffil.mty.itesm.mx%2Fmty%2Fferialibro%2Fprograma.pdf', '_blank', 'location=no');
+     //ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+     //ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
+     //ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+     //ref.addEventListener('exit', function(event) { alert(event.type); });
 }
 
 
